93934a4c6d9ff0e22eac0e797307487264478aa3
Grant Gordon <ggordon@aligntech.com>
2022-09-03 19:52:32 -0500

    fix issue where error would print on exit; change if/elif/else logic

--- days-to-hours.py
+++ days-to-hours.py
@@ -11,17 +11,19 @@ def validate_and_execute():
         if user_input_int > 0:
                 calculated_result = days_to_units(user_input_int)
                 print (calculated_result)
-        elif user_input_int == 0:
-            print("Please, enter a valid positive number!")
         else:
-            print ("Your input is not valid!")
+            print("Please, enter a valid positive number!")
     except ValueError:
         print("Your input is not valid!")
 
 
 user_input = ""
-while user_input != "exit":
+while True:
     user_input = input ("Hello user, please... Insert a number of days and I will convert it to hours!\nAfter you finished your calculation enter 'exit' to finish the program: ")
+
+    if user_input == "exit":
+        break
+
     list_of_days = user_input.split(", ")
 
     for num_of_days_element in set(list_of_days):
